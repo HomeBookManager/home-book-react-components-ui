551b8051006b71def348e5d1db713e36bb67f314
ErykSandeckiIntive <eryk.sandecki@intive.com>
2021-10-07 22:59:18 +0200

    feat: update rollupConfig & create service to resolve svg problem

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,14 @@
 import commonjs from '@rollup/plugin-commonjs';
+import image from '@rollup/plugin-image';
 import postcss from 'rollup-plugin-postcss';
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import visualizer from 'rollup-plugin-visualizer';
 import { getFiles } from './scripts/buildUtils';
 import { terser } from 'rollup-plugin-terser';
+
+// services
+import rollupPluginSvg from './src/services/library/rollup-plugin-svg';
 
 const extensions = ['.js', '.ts', '.jsx', '.tsx'];
 const excludeExtensions = ['.stories.tsx'];
@@ -22,6 +26,8 @@ export default {
     sourcemap: true,
   },
   plugins: [
+    image(),
+    rollupPluginSvg(),
     typescript({
       tsconfig: './tsconfig.build.json',
       declaration: true,
