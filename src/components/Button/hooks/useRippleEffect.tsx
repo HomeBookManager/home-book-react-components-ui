8c322553657b9ebd87d5dc70c3c9ce2b296d467a
ErykSandeckiIntive <eryk.sandecki@intive.com>
2021-10-08 15:30:41 +0200

    feat: refactoring of rippleEffect

--- src/components/Button/hooks/useRippleEffect.tsx
+++ src/components/Button/hooks/useRippleEffect.tsx
@@ -1,29 +1,48 @@
-// @ts-nocheck
 import { MouseEvent, useCallback } from 'react';
 
 export const useRippleEffect = (): ((
   event: MouseEvent<HTMLButtonElement>
 ) => void) => {
+  const adjustHTMLSpanBeforeAppend = (
+    event: MouseEvent<HTMLButtonElement>,
+    htmlSpan: HTMLSpanElement
+  ) => {
+    const { style } = htmlSpan;
+    const {
+      clientY,
+      clientX,
+      currentTarget: { clientHeight, clientWidth, offsetLeft, offsetTop },
+    } = event;
+    const diameter = Math.max(clientWidth, clientHeight);
+    const radius = diameter / 2;
+
+    style.width = style.height = `${diameter}px`;
+    style.left = `${clientX - offsetLeft - radius}px`;
+    style.top = `${clientY - offsetTop - radius}px`;
+  };
+
+  const appendHTMLSpanToButton = (
+    event: MouseEvent<HTMLButtonElement>,
+    htmlSpan: HTMLSpanElement
+  ) => {
+    const { currentTarget } = event;
+    const { classList } = htmlSpan;
+    const ripple = currentTarget.getElementsByClassName('Button__ripple')[0];
+
+    classList.add('Button__ripple');
+
+    if (ripple) {
+      ripple.remove();
+    }
+    currentTarget.appendChild(htmlSpan);
+  };
+
   const clickHandler = useCallback(
     (event: MouseEvent<HTMLButtonElement>): void => {
-      const button = event.currentTarget;
-      const circle = document.createElement('span');
-      const { classList, style } = circle;
-      const diameter = Math.max(button.clientWidth, button.clientHeight);
-      const radius = diameter / 2;
+      const htmlSpan = document.createElement('span');
 
-      style.width = circle.style.height = `${diameter}px`;
-      style.left = `${event.clientX - button.offsetLeft - radius}px`;
-      style.top = `${event.clientY - button.offsetTop - radius}px`;
-      classList.add('Button__ripple');
-
-      const ripple = button.getElementsByClassName('Button__ripple')[0];
-
-      if (ripple) {
-        ripple.remove();
-      }
-
-      button.appendChild(circle);
+      adjustHTMLSpanBeforeAppend(event, htmlSpan);
+      appendHTMLSpanToButton(event, htmlSpan);
     },
     []
   );
