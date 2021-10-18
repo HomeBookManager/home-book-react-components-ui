7a99ca6478012e186a0a6e8e8b1a6357b17170a8
ErykSandeckiIntive <eryk.sandecki@intive.com>
2021-10-18 16:54:26 +0200

    feat: create files for input group

---
+++ src/components/ButtonGroup/ButtonGroup.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+
+// styles
+import './button-group.scss';
+
+export type TProps = {};
+
+export const ButtonGroup: FC<TProps> = () => {
+  return <></>;
+};
+
+export default ButtonGroup;
